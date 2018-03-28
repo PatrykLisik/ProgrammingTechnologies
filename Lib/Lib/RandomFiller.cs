9abdf1ec1f03e79ff126fc894e668984b0353611
PatrykLisik <patrykkbk@gmail.com>
2018-03-28 20:43:52 +0200

    Filler And Data Repository Efficiency Tests

---
+++ Lib/Lib/RandomFiller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class RandomFiller:IDataFiller
+    {
+        Random rng = new Random();
+        int amount;
+
+        public RandomFiller(int amount)
+        {
+            this.amount = amount;
+        }
+
+        String RandomString()
+        {
+            return Guid.NewGuid().ToString().Substring(0, 10);
+        }
+        Book RandomBook()
+        {
+            return new Book(RandomString(), RandomString());
+        }
+        Reader RandomReader()
+        {
+            return new Reader(RandomString(), RandomString(), rng.Next());
+        }
+        Borrow RandomBorrow()
+        {
+            return new Borrow(RandomReader(), RandomBookDescription());
+          
+        }
+        BookDescription RandomBookDescription()
+        {
+            return new BookDescription(RandomString(), RandomBook());
+        }
+
+        public DataContext FillAll()
+        {
+                DataContext dataContext = new DataContext();
+                //No line length limit suggested, so I went wild 
+                dataContext.Books = (Enumerable.Repeat(RandomBook(), amount)).ToList();
+                dataContext.Readers = (Enumerable.Repeat(RandomReader(), amount)).ToList();
+                dataContext.BookDescriptions = (Enumerable.Repeat(RandomBookDescription(), amount)).ToList();
+                dataContext.Borrows = new System.Collections.ObjectModel.ObservableCollection<Borrow>((Enumerable.Repeat(RandomBorrow(), amount)).ToList());
+                dataContext.NumberOfBooks = dataContext.BookDescriptions.Distinct().ToDictionary(x => x, x => rng.Next());
+                return dataContext;
+            }
+        
+    }
+}
---
+++ Lib/LibTest/FillEfficiencyTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibTest
+{
+    
+    [TestClass]
+    public class FillerAndDataRepositoryEfficiencyTest
+    {
+        //I can run it at 7 but out of memory exception may happen if google chrome is in use
+        const int MaxExponentiation = 6;
+        private IDataFiller SetUpRandomFiller(int amount)
+        {
+            IDataFiller filler = new RandomFiller(amount);
+            return filler;
+        }
+        private IDataFiller SetUpConstFiller(int amount)
+        {
+            IDataFiller filler = new ConstFiller(amount);
+            return filler;
+        }
+        private TimeSpan Time(Action toTime)
+        {
+            var timer = Stopwatch.StartNew();
+            toTime();
+            timer.Stop();
+            return timer.Elapsed;
+        }
+       void FillRepo(IDataFiller filler)
+        {
+            var Repo = new DataRepository();
+            Repo.SetFiller(filler);
+            Repo.Fill();
+        }
+        void FillerEfficiency(Func<int,IDataFiller> filler)
+        {
+            for (int i = 0; i < MaxExponentiation; i++)
+            {
+
+                var Fill = new Action(() => FillRepo(filler(9 * (int)Math.Pow(10, i))));
+                var timeSpan = Time(Fill);
+                Assert.AreEqual(-1, TimeSpan.Compare(timeSpan, TimeSpan.FromSeconds(100)));
+                Console.WriteLine(String.Format("TIME OF {0} TESTS {1}", i, timeSpan));
+            }
+        }
+        [TestMethod]
+        public void RandomFillerEfficiency()
+        {
+
+            FillerEfficiency(SetUpRandomFiller);
+
+
+        }
+        [TestMethod]
+        public void ConstFillerEfficiency()
+        {
+            FillerEfficiency(SetUpConstFiller);
+        }
+    }
+}
