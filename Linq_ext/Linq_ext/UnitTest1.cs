8cab6b9f8ad519a7e5c265b206a4b67d31095eff
shreq <shrek.kresh@o2.pl>
2018-05-14 10:34:00 +0200

    asd

--- Linq_ext/Linq_ext/UnitTest1.cs
+++ Linq_ext/Linq_ext/UnitTest1.cs
@@ -12,10 +12,31 @@
         [TestClass]
         public class UnitTest1
         {
-
-
             [TestMethod]
             public void Even()
+            {
+                List<int> numbers = Enumerable.Range(0, 20).ToList();
+                List<int> even = numbers.Where(x => x % 2 == 0).ToList();
+                foreach (int i in even)
+                {
+                    Console.WriteLine(i);
+                    Assert.AreEqual(i % 2, 0);
+                }
+            }
+
+            [TestMethod]
+            public void Order()
+            {
+                List<int> numbers = new List<int> { 1, 2, 0 };
+                List<int> sorted = numbers.OrderBy(x => x.ToString()).ToList();
+                for (int i = 0; i < 3; i++)
+                {
+                    Assert.AreEqual(i, sorted[i]);
+                }
+            }
+
+            [TestMethod]
+            public void Even2()
             {
                 //Test data
                 List<int> numbers = Enumerable.Range(0, 20).ToList();
@@ -32,8 +53,6 @@
                 foreach (int i in even2)
                     Console.WriteLine(i);
             }
-
-
         }
     }
 }
