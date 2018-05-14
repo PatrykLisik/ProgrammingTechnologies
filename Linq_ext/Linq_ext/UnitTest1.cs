2be932d61f4d6916cdb689269488cdb8ec1e4f15
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-14 10:35:51 +0200

    stash

--- Linq_ext/Linq_ext/UnitTest1.cs
+++ Linq_ext/Linq_ext/UnitTest1.cs
@@ -1,25 +1,35 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Linq_ext
+﻿namespace Linq_ext
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Xml.Serialization;
 
     namespace Ext_meth
     {
         [TestClass]
         public class UnitTest1
         {
+
+
             [TestMethod]
             public void Even()
             {
+                //Test data
                 List<int> numbers = Enumerable.Range(0, 20).ToList();
-                List<int> even = numbers.Where(x => x % 2 == 0).ToList();
-                foreach (int i in even)
-                {
+                Func<int,bool> foo = x => x % 2 == 0;
+                Predicate<int> pred = x => x % 2 == 0;
+
+                List<int> even1 = numbers.Where(x => x % 2 == 0).ToList();
+                List<int> even2 = numbers.Where(foo).ToList();
+
+                // bad cant compile
+                //List<int> even2 = numbers.Where(pred).ToList();
+
+                Assert.IsTrue(even1.All(even2.Contains));
+                foreach (int i in even2)
                     Console.WriteLine(i);
                     Assert.AreEqual(i%2, 0);
                 }
@@ -35,6 +45,8 @@ namespace Linq_ext
                     Assert.AreEqual(i, sorted[i]);
                 }
             }
+
+
         }
     }
 }
