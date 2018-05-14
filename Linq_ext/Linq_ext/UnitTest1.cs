1c766bb765a9e871e61c04a703dd71d776c60555
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-14 10:23:43 +0200

    more func

--- Linq_ext/Linq_ext/UnitTest1.cs
+++ Linq_ext/Linq_ext/UnitTest1.cs
@@ -19,29 +19,18 @@
             {
                 //Test data
                 List<int> numbers = Enumerable.Range(0, 20).ToList();
-
-                Func<int, bool> foo = x => x % 2 == 0;
+                Func<int,bool> foo = x => x % 2 == 0;
                 Predicate<int> pred = x => x % 2 == 0;
 
                 List<int> even1 = numbers.Where(x => x % 2 == 0).ToList();
                 List<int> even2 = numbers.Where(foo).ToList();
 
-                //Bad compile error
-                // List<int> even3 = numbers.Where(pred).ToList();
+                // bad cant compile
+                //List<int> even2 = numbers.Where(pred).ToList();
 
-                //Assert that list are same elemnt-iwse
                 Assert.IsTrue(even1.All(even2.Contains));
-
-                Console.WriteLine("List:");
                 foreach (int i in even2)
                     Console.WriteLine(i);
-
-
-            }
-
-            [TestMethod]
-            public void XML()
-            {
             }
 
 
