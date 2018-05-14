7ac42f917bda57413c176c840fb67bdce2b5cd4d
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-14 08:39:31 +0200

    init

---
+++ Linq_ext/Linq_ext/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Linq_ext")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Linq_ext")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9795d8b3-c667-4aa4-a732-78cef7f1d65f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Linq_ext/Linq_ext/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Linq_ext
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    namespace Ext_meth
+    {
+        [TestClass]
+        public class UnitTest1
+        {
+            [TestMethod]
+            public void Even()
+            {
+                List<int> numbers = Enumerable.Range(0, 20).ToList();
+                List<int> even = numbers.Where(x => x % 2 == 0).ToList();
+                foreach (int i in even)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
+}
